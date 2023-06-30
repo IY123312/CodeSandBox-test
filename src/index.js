9aab73a0d147461c1d6224286dbb46a60b597298
Yutaro Itokawa <131341171+IY123312@users.noreply.github.com>
2023-06-30 15:23:35 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+console.log("aaa");
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
